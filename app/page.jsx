805c8e9b079852db9dfbed66d7cbb53a21a025b7
Gokul Krishna Gopakumar <gokulkrishnagopakumar@gmail.com>
2024-06-14 00:48:55 +0530

    updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,34 @@
-import Header from "@/components/Header";
+/*import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import StyleGuide from "@/components/StyleGuide";
 import Menu from "@/components/Menu";
 import Reservation from "@/components/Reservation";
 import About from "@/components/About";
 import Map from "@/components/Map";
-import Footer from "@/components/Footer";
+import Footer from "@/components/Footer";*/
+import dynamic from "next/dynamic";
+
+const DynamicHeader = dynamic(() => import("@/components/Header"), {
+  ssr: false,
+});
+const DynamicHero = dynamic(() => import("@/components/Hero"), {
+  ssr: false,
+});
+const DynamicMenu = dynamic(() => import("@/components/Menu"), {
+  ssr: false,
+});
+const DynamicReservation = dynamic(() => import("@/components/Reservation"), {
+  ssr: false,
+});
+const DynamicAbout = dynamic(() => import("@/components/About"), {
+  ssr: false,
+});
+const DynamicMap = dynamic(() => import("@/components/Map"), {
+  ssr: false,
+});
+const DynamicFooter = dynamic(() => import("@/components/Footer"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
@@ -13,13 +36,13 @@ export default function Home() {
       className=" w-full max-w-[1440px] bg-white mx-auto
             overflow-hidden"
     >
-      <Header />
-      <Hero />
-      <Menu />
-      <Reservation />
-      <About />
-      <Map />
-      <Footer />
+      <DynamicHeader />
+      <DynamicHero />
+      <DynamicMenu />
+      <DynamicReservation />
+      <DynamicAbout />
+      <DynamicMap />
+      <DynamicFooter />
       {/* <div className="h-[4000px]"></div> */}
     </main>
   );
